7338b4ed6d98287d2c96ffef7d6450161c34d4a4
xinchen <xinchen@gabbasoft.dev>
2025-05-17 14:39:17 +0700

    feat: implemented lib for bitcoin

---
+++ lib/src/types/mod.rs
@@ -0,0 +1,25 @@
+pub mod block;
+pub mod blockchain;
+pub mod transaction;
+
+use {
+    crate::{
+        U256,
+        crypto::{PublicKey, Signature},
+        error::{BtcError, Result},
+        sha256::Hash,
+        util::{MerkleRoot, Saveable},
+    },
+    bigdecimal::BigDecimal,
+    chrono::{DateTime, Utc},
+    std::{
+        collections::{HashMap, HashSet},
+        io::{Error as IoError, ErrorKind as IoErrorKind, Read, Result as IoResult, Write},
+    },
+    uuid::Uuid,
+};
+pub use {
+    block::Block,
+    blockchain::Blockchain,
+    transaction::{Transaction, TransactionInput, TransactionOutput},
+};
